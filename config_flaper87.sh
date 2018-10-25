f9d6ce711e1c3b67ab0a08dfa3ac2799f7af7d41
Flavio Percoco <flaper87@gmail.com>
2018-10-25 05:25:40 +0000

    Add flaper87 configs

---
+++ config_flaper87.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+LOCAL_IP=10.1.8.88
+CIDR=22
+NETWORK=10.1.8
+DEFAULT_ROUTE=10.1.11.254
+PUBLIC_INTERFACE=em1
+
+# Needed if running in a VM
+PARAMETERS_EXTRA="NtpServer: ['clock.redhat.com']"
