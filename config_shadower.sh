cfd4d186af26971c9b24b77924ee3546b09c0481
trown <trown@redhat.com>
2018-10-29 16:33:19 -0400

    Fix DNS server in configs

--- config_shadower.sh
+++ config_shadower.sh
@@ -5,7 +5,7 @@ CIDR=22
 NETWORK=10.1.8
 DEFAULT_ROUTE=10.1.11.254
 PUBLIC_INTERFACE=em1
-DNS_SERVER_1=10.11.5.9
+DNS_SERVER_1=10.11.5.19
 
 # Needed if running in a VM
 PARAMETERS_EXTRA="NtpServer: ['clock.redhat.com']"
