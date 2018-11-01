76cd1eae7af2ed6a97db83292c14eff32029b707
trown <trown@redhat.com>
2018-11-01 10:44:32 -0400

    Add workaround for hardcoded /etc/openstack/clouds.yaml path

--- 05_install_ocp.sh
+++ 05_install_ocp.sh
@@ -20,4 +20,12 @@ export OPENSHIFT_INSTALL_OPENSTACK_EXTERNAL_NETWORK=public
 export OPENSHIFT_INSTALL_PULL_SECRET='{"auths": { "quay.io": { "auth": "Y29yZW9zK3RlYzJfaWZidWdsa2VndmF0aXJyemlqZGMybnJ5ZzpWRVM0SVA0TjdSTjNROUUwMFA1Rk9NMjdSQUZNM1lIRjRYSzQ2UlJBTTFZQVdZWTdLOUFIQlM1OVBQVjhEVlla", "email": "" }}}'
 export OPENSHIFT_INSTALL_SSH_PUB_KEY="`cat $HOME/.ssh/id_rsa.pub`"
 
+# NOTE(trown): It seems like there is a bug with hardcoded path for the clouds.yaml file in manifests/tectonic.go
+# https://github.com/openshift/installer/blob/2b52ad20793d37471c5645cbbe089e8a6656b802/pkg/asset/manifests/tectonic.go#L23
+# We can workaround the bug by copying our clouds.yaml there
+if ! [ -f /etc/openstack/clouds.yaml ]; then
+    sudo mkdir /etc/openstack
+    sudo cp $HOME/.config/openstack/clouds.yaml /etc/openstack/
+fi
+
 $GOPATH/src/github.com/openshift/installer/bin/openshift-install --log-level=debug cluster --dir ocp
