d03b9ce0f91ffed16f23ba3f957c2dbf21dc3cae
Flavio Percoco <flaper87@gmail.com>
2018-11-06 05:25:19 +0000

    Link to fix

--- 05_install_ocp.sh
+++ 05_install_ocp.sh
@@ -23,9 +23,12 @@ export OPENSHIFT_INSTALL_SSH_PUB_KEY="`cat $HOME/.ssh/id_rsa.pub`"
 # NOTE(trown): It seems like there is a bug with hardcoded path for the clouds.yaml file in manifests/tectonic.go
 # https://github.com/openshift/installer/blob/2b52ad20793d37471c5645cbbe089e8a6656b802/pkg/asset/manifests/tectonic.go#L23
 # We can workaround the bug by copying our clouds.yaml there
+# This will be fixed by https://github.com/openshift/installer/pull/588
 if ! [ -f /etc/openstack/clouds.yaml ]; then
     sudo mkdir /etc/openstack
     sudo cp $HOME/.config/openstack/clouds.yaml /etc/openstack/
 fi
 
 $GOPATH/src/github.com/openshift/installer/bin/openshift-install --log-level=debug cluster --dir ocp
+
+openstack port set --dns-name $OPENSHIFT_INSTALL_CLUSTER_NAME-api bootstrap-port
