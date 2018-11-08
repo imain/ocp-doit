30d7cba371f4fa558e41ce99535d8460e8eebba9
Tomas Sedovic <tomas@sedovic.cz>
2018-11-08 12:15:47 +0100

    Don't fail cleanup when there are no volumes
    By default, xargs will always run the command at least once. If there
    are no volumes present, docker will exit with a failure.
    
    Adding the `--no-run-if-empty` argument will not run docker if there are
    no volumes to delete.

--- cleanup_be_careful.sh
+++ cleanup_be_careful.sh
@@ -5,7 +5,7 @@ set -x
 sudo docker ps -aq | xargs sudo docker stop
 sudo docker ps -aq | xargs sudo docker rm -f
 
-sudo docker volume ls -q | xargs sudo docker volume rm
+sudo docker volume ls -q | xargs --no-run-if-empty sudo docker volume rm
 sudo rm -Rf /var/lib/mysql
 sudo rm -Rf /var/lib/rabbitmq
 sudo rm -Rf /var/lib/heat-config/*
