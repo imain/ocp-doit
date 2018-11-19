608d7caf2e8da8b1129f99165eac7e43d197ecc0
Flavio Percoco <flaper87@gmail.com>
2018-11-19 14:31:46 +0000

    Add dns patch to the repo sync

--- 04_ocp_repo_sync.sh
+++ 04_ocp_repo_sync.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -ex
+source common.sh
 
 eval "$(go env)"
 echo "$GOPATH" | lolcat # should print $HOME/go or something like that
@@ -17,8 +18,14 @@ git checkout master
 git branch -D we_dont_need_no_stinkin_patches || true
 git checkout -b we_dont_need_no_stinkin_patches
 
-echo https://github.com/openshift/installer/pull/611 | lolcat
-curl -L https://github.com/openshift/installer/pull/611.patch | git am
+#echo https://github.com/openshift/installer/pull/611 | lolcat
+#curl -L https://github.com/openshift/installer/pull/611.patch | git am
+patch_file=$(mktemp)
+echo $patch_file
+curl -L https://github.com/tomassedovic/installer/commit/a76d997ff7736236132ab3cea094b6dde1004692.patch -o $patch_file 
+sed -i "s/10.1.11.152/$BOOTSTRAP_FLOATING_IP/g" $patch_file
+git am < $patch_file
+
 
 
 figlet "Syncing Terraform Repos" | lolcat
--- config_flaper87.sh
+++ config_flaper87.sh
@@ -9,5 +9,7 @@ DEFAULT_ROUTE=10.1.11.254
 PUBLIC_INTERFACE=em1
 DNS_SERVER_1=10.11.5.19
 
+BOOTSTRAP_FLOATING_IP=10.1.11.181
+
 # Needed if running in a VM
 PARAMETERS_EXTRA="NtpServer: ['clock.redhat.com']"
