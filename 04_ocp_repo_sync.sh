c837d05b271d7b098275317a74082ef7e6ff32ee
Tomas Sedovic <tomas@sedovic.cz>
2018-11-21 18:09:40 +0100

    Use branch name for the DNS workaround patch
    That lets us update the branch without having to make any changes to
    this repo. The latest commit removes the race condition from disabling
    the master kubelet openstack cloud providier.

--- 04_ocp_repo_sync.sh
+++ 04_ocp_repo_sync.sh
@@ -22,7 +22,7 @@ git checkout -b we_dont_need_no_stinkin_patches
 #curl -L https://github.com/openshift/installer/pull/611.patch | git am
 patch_file=$(mktemp)
 echo $patch_file
-curl -L https://github.com/tomassedovic/installer/commit/a76d997ff7736236132ab3cea094b6dde1004692.patch -o $patch_file 
+curl -L https://github.com/tomassedovic/installer/commit/dns-workaround.patch -o $patch_file
 sed -i "s/10.1.11.152/$BOOTSTRAP_FLOATING_IP/g" $patch_file
 git am < $patch_file
 
