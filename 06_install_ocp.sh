fa1930576bf11fee28bd24f3f0b5e15a91c95cfe
Flavio Percoco <flaper87@gmail.com>
2018-11-29 11:03:07 +0000

    Don't set the ostest-bootstrap dns-name

--- 06_install_ocp.sh
+++ 06_install_ocp.sh
@@ -6,5 +6,3 @@ source common.sh
 source ocp_install_env.sh
 
 $GOPATH/src/github.com/openshift/installer/bin/openshift-install --log-level=debug create cluster --dir ocp
-
-openstack port set --dns-name $OPENSHIFT_INSTALL_CLUSTER_NAME-api bootstrap-port
