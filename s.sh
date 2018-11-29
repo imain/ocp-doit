1bc178bd6377f27ac7b1e342b8610afd7e54933a
Flavio Percoco <flaper87@gmail.com>
2018-11-29 11:03:54 +0000

    Add s.sh script to ssh into VMs without FIPs
    Thanks shadowers, this is an awesome trick!

---
+++ s.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source ocp_install_env.sh
+
+NODE_ADDRESSES=`openstack server show ostest-$1 -f value -c addresses | cut -d',' -f1`
+NODE_IP=${NODE_ADDRESSES#"openshift="}
+ip netns exec "qdhcp-$(openstack --os-cloud standalone network show openshift -f value -c id)" ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" core@$NODE_IP
