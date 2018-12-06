1e79258d4dde78ecd4b59b423dc47d0073a6e905
Steven Hardy <shardy@redhat.com>
2018-12-06 10:19:26 +0000

    Move image variables into common.sh
    The recent version bump only updated get_rhcos_image.sh
    so we'll calculate the image hash in 03_configure_undercloud.sh
    with the old filename.
    
    Probably best to move the variables to the common.sh, and I also
    removed the lolcat/figlet usage as I need to run get_rhcos_image.sh
    on a different box without all the dependencies installed, so lets
    keep the requirements as minimal as possible.

--- get_rhcos_image.sh
+++ get_rhcos_image.sh
@@ -3,9 +3,6 @@ set -xe
 
 source common.sh
 
-RHCOS_IMAGE_VERSION="${RHCOS_IMAGE_VERSION:-47.188}"
-RHCOS_IMAGE_NAME="redhat-coreos-maipo-${RHCOS_IMAGE_VERSION}"
-RHCOS_IMAGE_FILENAME="${RHCOS_IMAGE_NAME}-openstack.qcow2"
 if [ ! -f "$RHCOS_IMAGE_FILENAME" ]; then
     curl --insecure --compressed -L -o "${RHCOS_IMAGE_FILENAME}" "https://releases-redhat-coreos-dev.cloud.paas.upshift.redhat.com/storage/releases/maipo/${RHCOS_IMAGE_VERSION}/${RHCOS_IMAGE_FILENAME}".gz
 fi
