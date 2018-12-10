4ecb2a7342fe58080fc5d8d93abd9c0e02870a69
Brad P. Crochet <brad@redhat.com>
2018-12-10 16:08:26 -0500

    Use ansible

--- 01_install_requirements.sh
+++ 01_install_requirements.sh
@@ -3,49 +3,6 @@ set -x
 
 source common.sh
 
-sudo setenforce permissive
+sudo yum -y install ansible
 
-sudo yum -y update
-
-sudo yum -y erase golang-src
-
-sudo yum -y install http://cbs.centos.org/kojifiles/packages/go-srpm-macros/2/17.el7/noarch/go-srpm-macros-2-17.el7.noarch.rpm
-sudo yum -y install $GOLANG_SOURCE/noarch/golang-src-$GOLANG_VERSION.noarch.rpm \
-                    $GOLANG_SOURCE/x86_64/golang-$GOLANG_VERSION.x86_64.rpm \
-                    $GOLANG_SOURCE/x86_64/golang-bin-$GOLANG_VERSION.x86_64.rpm
-
-sudo yum -y install epel-release
-sudo yum -y install curl vim-enhanced wget python-pip patch psmisc figlet golang
-sudo yum -y install https://dprince.fedorapeople.org/tmate-2.2.1-1.el7.centos.x86_64.rpm
-
-sudo pip install lolcat
-
-# for tripleo-repos install:
-sudo yum -y install python-setuptools python-requests
-
-if [ ! -f openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz ]; then
-  wget https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
-  tar xvzf openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
-  sudo cp openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit/{kubectl,oc} /usr/local/bin/
-fi
-cd
-git clone https://git.openstack.org/openstack/tripleo-repos
-cd tripleo-repos
-sudo python setup.py install
-cd
-#sudo tripleo-repos current
-#sudo tripleo-repos current-tripleo
-
-# current-tripleo is broken for the all-in-one atm.
-sudo tripleo-repos current-tripleo
-sudo yum -y update
-sudo yum install -y python2-tripleoclient
-
-# TRIPLEO HEAT TEMPLATES
-if [ ! -d $SCRIPTDIR/tripleo-heat-templates ]; then
-  cd $SCRIPTDIR
-  cp -rv /usr/share/openstack-tripleo-heat-templates ./tripleo-heat-templates
-  cd tripleo-heat-templates
-  cat ../octavia_hack.patch | patch -p1
-fi
-
+ansible-playbook install_requirements.yaml
--- 02_run_all_in_one.sh
+++ 02_run_all_in_one.sh
@@ -5,62 +5,8 @@ source common.sh
 : ${DNS_SERVER_1:=1.1.1.1}
 : ${DNS_SERVER_2:=8.8.8.8}
 
-openstack tripleo container image prepare default \
-      --output-env-file $SCRIPTDIR/containers-prepare-parameters.yaml
-
-cat > $SCRIPTDIR/standalone_parameters.yaml <<-EOF_CAT
-parameter_defaults:
-  CloudName: $LOCAL_IP
-  # default gateway
-  ControlPlaneStaticRoutes:
-   - ip_netmask: 0.0.0.0/0
-     next_hop: $DEFAULT_ROUTE
-     default: true
-  Debug: true
-  DeploymentUser: $USER
-  DnsServers:
-    - $DNS_SERVER_1
-    - $DNS_SERVER_2
-  NeutronDhcpAgentDnsmasqDnsServers:
-    - $DNS_SERVER_1
-    - $DNS_SERVER_2
-  # needed for vip & pacemaker
-  KernelIpNonLocalBind: 1
-  DockerInsecureRegistryAddress:
-  - $LOCAL_IP:8787
-  NeutronPublicInterface: $PUBLIC_INTERFACE
-  # re-use ctlplane bridge for public net
-  NeutronBridgeMappings: datacentre:br-ctlplane
-  NeutronPhysicalBridge: br-ctlplane
-  # enable to force metadata for public net
-  NeutronEnableForceMetadata: true
-  NeutronDnsDomain: shiftstack.com
-  NeutronPluginExtensions: "qos,port_security,dns_domain_ports"
-  ControllerExtraConfig:
-    neutron::agents::dhcp::dnsmasq_local_resolv: true
-
-  StandaloneEnableRoutedNetworks: false
-  StandaloneHomeDir: /home/$USER
-  StandaloneLocalMtu: 1500
-  $PARAMETERS_EXTRA
-EOF_CAT
-
-lolcat $SCRIPTDIR/standalone_parameters.yaml
-
-sudo openstack tripleo deploy \
-    --templates $SCRIPTDIR/tripleo-heat-templates \
-    --local-ip=$LOCAL_IP/$CIDR \
-    -e $SCRIPTDIR/containers-prepare-parameters.yaml \
-    -r $SCRIPTDIR/standalone.yaml \
-    -e $SCRIPTDIR/standalone_parameters.yaml \
-    -e $SCRIPTDIR/tripleo-heat-templates/environments/standalone.yaml \
-    --output-dir $SCRIPTDIR/standalone \
-    --standalone
+ansible-playbook -e "local_ip=$LOCAL_IP cidr=$CIDR default_route=$DEFAULT_ROUTE public_interface=$PUBLIC_INTERFACE parameters_extra='$PARAMETERS_EXTRA' dns_server_1=$DNS_SERVER_1 dns_server_2=$DNS_SERVER_2" run_all_in_one.yaml
 
 # NOTE(flaper87): We're using tripleo-current,
 # and this template only exists on master
 # -e $SCRIPTDIR/tripleo-heat-templates/environments/standalone/standalone-tripleo.yaml \
-
-sudo chown -R $USER:$USER ~/.config/openstack
-sed -i.bak 's/cloud:/#cloud:/' ~/.config/openstack/clouds.yaml
-sed -i.bak '4i\      domain_name: default' ~/.config/openstack/clouds.yaml
