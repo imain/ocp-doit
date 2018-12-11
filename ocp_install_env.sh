e920a253dd692206de200d3adc37bed1e5bb8e07
Russell Bryant <rbryant@redhat.com>
2018-12-11 16:15:01 -0500

    s.sh: Pass ssh identity argument.
    This script did not work for me without this argument present.

--- ocp_install_env.sh
+++ ocp_install_env.sh
@@ -14,6 +14,9 @@ export OPENSHIFT_INSTALL_OPENSTACK_EXTERNAL_NETWORK=public
 export OPENSHIFT_INSTALL_PULL_SECRET='{"auths": { "quay.io": { "auth": "Y29yZW9zK3RlYzJfaWZidWdsa2VndmF0aXJyemlqZGMybnJ5ZzpWRVM0SVA0TjdSTjNROUUwMFA1Rk9NMjdSQUZNM1lIRjRYSzQ2UlJBTTFZQVdZWTdLOUFIQlM1OVBQVjhEVlla", "email": "" }}}'
 export OPENSHIFT_INSTALL_SSH_PUB_KEY="`cat $HOME/.ssh/id_rsa.pub`"
 
+# Not used by the installer.  Used by s.sh.
+export OPENSHIFT_INSTALL_SSH_PRIV_KEY="$HOME/.ssh/id_rsa"
+
 # NOTE(trown): It seems like there is a bug with hardcoded path for the clouds.yaml file in manifests/tectonic.go
 # https://github.com/openshift/installer/blob/2b52ad20793d37471c5645cbbe089e8a6656b802/pkg/asset/manifests/tectonic.go#L23
 # We can workaround the bug by copying our clouds.yaml there
