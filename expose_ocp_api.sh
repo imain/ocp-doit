413a004ecb38a343aaabfff15fdc4f68a87ff24c
Russell Bryant <rbryant@redhat.com>
2018-12-17 16:53:00 +0000

    Update env var name.

--- expose_ocp_api.sh
+++ expose_ocp_api.sh
@@ -18,7 +18,7 @@ fi
 echo "Attempting to expose cluster's API on floating IP: ${LB_FLOATING_IP}" | lolcat
 
 if ! openstack floating ip list | grep ${LB_FLOATING_IP} ; then
-    openstack floating ip create --floating-ip-address ${LB_FLOATING_IP} ${OPENSHIFT_INSTALL_OPENSTACK_EXTERNAL_NETWORK}
+    openstack floating ip create --floating-ip-address ${LB_FLOATING_IP} ${OPENSTACK_EXTERNAL_NETWORK}
 fi
 openstack floating ip set --port lb-port ${LB_FLOATING_IP}
 
