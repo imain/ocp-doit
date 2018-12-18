0cdbfe10dba441ee98d81055bd728f4d8f12760d
John Trowbridge <trown@redhat.com>
2018-12-18 14:38:28 +0000

    Update expose_ocp_api.sh script
    We now have support in the installer for creating and destroying
    the FIP and attaching it to our service VM. We still need to put
    an entry in /etc/hosts on the machine running openshift/installer,
    but that is all we need now.

--- expose_ocp_api.sh
+++ expose_ocp_api.sh
@@ -10,18 +10,10 @@ set -ex
 source ocp_install_env.sh
 source common.sh
 
-if [ "${LB_FLOATING_IP}" = "" ] ; then
-    echo "Please set LB_FLOATING_IP in your config." | lolcat
-    exit 1
-fi
+LB_FLOATING_IP=$(openstack server show ostest-api -f value -c addresses | cut -d " " -f 2)
 
 echo "Attempting to expose cluster's API on floating IP: ${LB_FLOATING_IP}" | lolcat
 
-if ! openstack floating ip list | grep ${LB_FLOATING_IP} ; then
-    openstack floating ip create --floating-ip-address ${LB_FLOATING_IP} ${OPENSTACK_EXTERNAL_NETWORK}
-fi
-openstack floating ip set --port lb-port ${LB_FLOATING_IP}
-
 if ! grep -q ${LB_FLOATING_IP} /etc/hosts ; then
-    (echo "${LB_FLOATING_IP} ostest-api.shiftstack.com" && cat /etc/hosts) | sudo tee /etc/hosts
+    (echo "${LB_FLOATING_IP} ostest-api.shiftstack.com" && grep -v "ostest-api.shiftstack.com" /etc/hosts) | sudo tee /etc/hosts
 fi
