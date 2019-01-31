c5f7293cfb53f09f761a7b97fee3cd63a118ebe0
Flavio Percoco <flaper87@gmail.com>
2019-01-31 10:13:39 +0100

    Force CI release image

--- common.sh
+++ common.sh
@@ -18,6 +18,12 @@ export RHCOS_IMAGE_VERSION="${RHCOS_IMAGE_VERSION:-47.188}"
 export RHCOS_IMAGE_NAME="redhat-coreos-maipo-${RHCOS_IMAGE_VERSION}"
 export RHCOS_IMAGE_FILENAME="${RHCOS_IMAGE_NAME}-openstack.qcow2"
 
+# NOTE(flaper87): Hardcoding the CI release image until we stabilize OpenStack based deployments.
+# This forces the installer to use the latest release image built in CI and, therefore, pick the latest
+# images built for every component. There should not be propagation delays for component's images when
+# using this release image.
+export OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE="registry.svc.ci.openshift.org/openshift/origin-release:v4.0"
+
 # We are pinning to upstream tripleo version.  This has passed CI promotion.
 # We'll have to check once in a while for a new version though.
 TRIPLEO_VERSION='7e28b872afba83ed4b3789b14d4d3c5061789ace_c3783cbe'
