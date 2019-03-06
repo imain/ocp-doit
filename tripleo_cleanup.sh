a9ee4c69ce5ab98e6a62fed6cb3545648a64b5eb
Martin André <m.andre@redhat.com>
2019-03-06 19:08:42 +0100

    Remove more things with tripleo_cleanup
    Residual files may cause the 02_run_all_in_one.sh script to fail when
    re-installing the standalone openstack.

--- tripleo_cleanup.sh
+++ tripleo_cleanup.sh
@@ -6,7 +6,19 @@ sudo docker ps -aq | xargs sudo docker stop
 sudo docker ps -aq | xargs sudo docker rm -f
 
 sudo docker volume ls -q | xargs --no-run-if-empty sudo docker volume rm
-sudo rm -Rf /var/lib/mysql
-sudo rm -Rf /var/lib/rabbitmq
+sudo rm -Rf /var/lib/config-data
+sudo rm -Rf /var/lib/docker-puppet/
+sudo rm -Rf /var/lib/glance
 sudo rm -Rf /var/lib/heat-config/*
+sudo rm -Rf /var/lib/kolla
+sudo rm -Rf /var/lib/mysql
+sudo rm -Rf /var/lib/neutron
+sudo rm -Rf /var/lib/nova
+sudo rm -Rf /var/lib/openstack
+sudo rm -Rf /var/lib/puppet
+sudo rm -Rf /var/lib/rabbitmq
+sudo rm -Rf /var/lib/tripleo
+sudo rm -Rf /var/lib/tripleo-config
+sudo rm -Rf /var/lib/tripleo-heat-installer
 sudo rm -Rf standalone
+sudo rm /etc/my.cnf.d/tripleo.cnf
