3c93bb7f1fdbe232e4b5221f5f2ad7032cd75d44
Flavio Percoco <flaper87@gmail.com>
2019-03-08 13:29:51 +0100

    Clean worker's ports and servers until we fix the issue in the installer

--- ocp_cleanup.sh
+++ ocp_cleanup.sh
@@ -4,6 +4,9 @@ set -e
 
 source ocp_install_env.sh
 
+openstack port list -c name -f value | grep $CLUSTER_NAME | grep worker | xargs openstack port delete || true
+openstack server list -c Name -f value | grep $CLUSTER_NAME | grep worker | xargs openstack server delete || true
+
 $GOPATH/src/github.com/openshift/installer/bin/openshift-install --log-level=debug --dir $CLUSTER_NAME destroy cluster
 
 # clean /etc/hosts
